e0dc6651bd14d57c44820f42a30e70baf4a4c352
BakerRaimond <34089609+BakerRaimond@users.noreply.github.com>
2017-12-13 10:43:44 +0200

    Create ma.py

---
+++ ma.py
@@ -0,0 +1,6 @@
+from math import pi
+
+print('Ringi diameeter on ' + str(2 * 60.25) + ' cm')
+print('Ümbermõõt on ' + str(pi * 2 * 60.25) + ' cm')
+print('Pindala on ' + str(pi * (60.25 ** 2)) + ' cm2')
+
